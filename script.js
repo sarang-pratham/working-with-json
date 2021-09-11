2a9db78dbb57e5270568ed43ac8c3261bac83a76
Sarang Pratham <68158794+sarang-pratham@users.noreply.github.com>
2021-09-11 20:28:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const table = document.querySelector("table");
+
+let names = [];
+let students;
+fetch("studMarks.json")
+	.then((response) => {
+		return response.json();
+	})
+	.then((data) => {
+		students = data;
+		data.forEach((element) => {
+			if (!names.includes(element.Name)) {
+				names.push(element.Name);
+			}
+		});
+		names.forEach((name) => {
+			let mark = students.reduce((acc, curr) => {
+				if (name == curr.Name) {
+					acc += curr.Marks;
+				}
+				return acc;
+			}, 0);
+
+			// console.log(name, mark);
+			let str = `<tr><td>${name}</td>
+			<td>${mark}</td></tr>`;
+			table.innerHTML += str;
+		});
+	});
